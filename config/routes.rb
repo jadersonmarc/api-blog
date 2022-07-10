b292d2aeb94e2f19dd8e26cb79650580250b0174
jadersonmarc <jadersonmarc@gmail.com>
2022-07-09 22:11:41 -0300

    add routes and path controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+      post :auth, to: "authentication#create"
+      namespace :articles, path: "/" do
+        resources :articles
+      end
+  end
+
+  root to: "api/articles/articles#index"
 end
